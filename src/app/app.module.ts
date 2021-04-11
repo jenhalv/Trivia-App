c916a946530e9932b9fa3d5fdfd6e21b87042153
Jen <jenhalv06@gmail.com>
2021-04-10 20:24:34 -0500

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TriviaComponent } from './trivia/trivia.component';
+import { Route, RouterModule } from '@angular/router';
+
+const routes: Route[] = [
+  {
+  path: 'trivia',
+  component: TriviaComponent
+  },
+  {
+    path: '',
+    component: TriviaComponent
+  },
+  {
+  path: '**',
+  component: TriviaComponent
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TriviaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
